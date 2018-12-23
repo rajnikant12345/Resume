08a5d944ae9cba35a4b88ed7958ce87e3144b103
rajnikant <rajnikant12345@gmail.com>
2018-12-24 00:27:03 +0530

    second commit

--- app/home.go
+++ app/home.go
@@ -27,6 +27,19 @@ func CreateHome()*engine.Element {
 			</li>
     </ul> 
 	</div>
+
+
+	<h1 class="f3 lh-title">Education</h1>
+    <div class="f5 dark-gray fw2 tracked">
+	<ul >
+  		<li>B.E. Electronics and Communication (Kumaon Enginerring College 2006 Passout) 67%</li>
+  		<li>XII CBSE, DAV Merrut, 73.8%</li>
+  		<li>X CBSE, DAV Meerut, 67.6%</li>
+	</ul> 
+	</div>
+
+	
+
 	<h1 class="f3 lh-title">My Strengths</h1>
 	<div class="f5 dark-gray fw2 tracked">
 	<ul >
@@ -136,10 +149,35 @@ func PdbCtl(i []js.Value) {
 	Body.AddChild(e)
 }
 
+func Cng(i []js.Value) {
+	Body.RemoveChild()
+	e := engine.NewElement("div")
+	e.SetInnerHtml(`
+<article class="athelas">
+  <div class="center measure-wide f3 lh-copy ph2">
+    <h1 class="f3 lh-title">CSP and CNG With Key Secure</h1>
+    <p class="f5 dark-gray fw2 tracked">
+		Used Microsoft SDK for CSP and CNG and developed an interface with Safenet Keysecure. Now this CSP and CNG can be used with tools like, SignTool, ADCS, ADRMS, IIS all these
+		software use key for encryption or signing and Using Safenet CNG and CSP, key always resides in KeySecure.
+    </p>
+	<h1 class="f3 lh-title">Technlogies Used</h1>
+	<div class="f5 dark-gray fw2 tracked">
+	<ul >
+  		<li>C, C++</li>
+  		<li>Windows</li>
+		<li>CSP, CNG</li>
+	</ul> 
+	</div>
+  </div>
+</article>`)
+	Body.AddChild(e)
+}
+
 func RegisterCallbacks() {
 	println("callback registered")
 	js.Global().Set("kylo", js.NewCallback(Kylo))
 	js.Global().Set("pdbctl", js.NewCallback(PdbCtl))
+	js.Global().Set("cng", js.NewCallback(Cng))
 }
 
 
@@ -150,39 +188,59 @@ func CreateOrgs()*engine.Element {
 	e.SetInnerHtml(`<article class="athelas">
 	<div class="center measure-wide f3 lh-copy ph2">
 	<h1 class="f3 lh-title">Gemlato Noida ( April 2016 ---- Present )</h1>
-	<div class="f5 dark-gray fw2 tracked nested-list-reset">
+	<div class="f5 dark-gray fw2 tracked">
 	<ul >
 	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray" onClick="kylo();">Next Generation Key Management Solution</a></li>
 	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray" onClick="pdbctl();">Database Migration Utility</a></li>
 	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray" onClick="cng();">Microsoft CNG(Cryptography API: Next Generation)</a></li>
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray" onClick="csp();">Microsoft CSP(Cryptographic Service Provider)</a></li>
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray" onClick="openssl();">OPENSSL engine for EToken</a></li>
+	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray" onClick="cng();">Microsoft CSP(Cryptographic Service Provider)</a></li>
+	<li>OPENSSL engine for EToken (C , C++, OpenSSL, Linux, PKCS11)</a></li>
 	</ul>
 	</div>
 	</div>
 
 	<div class="center measure-wide f3 lh-copy ph2">
 	<h1 class="f3 lh-title">Samsung Noida ( Feb 2013 ---- March 2016 )</h1>
-	<div class="f5 dark-gray fw2 tracked nested-list-reset">
-	<ul >
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray">SIEM Tool for Samsung R&D</a></li>
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray">ToyBox Project</a></li>
+	<div class="f5 dark-gray fw2 tracked">
+	<ul>
+	<li>Development of Open source command s for Samsung, ip link, ip route, ip neighbour, ftpd, dhcp server. (C, Sockets, Open Source, Linux)</li>
+	<li>Development of data collection framework, for Samsung products. Collects data from different Samsung products and send it to a common server. (C++, Linux, Windows, Tizen, Design Patterns)</li>
 	</ul>
 	</div>
 	</div>
 
 	<div class="center measure-wide f3 lh-copy ph2">
-	<h1 class="f3 lh-title">Symantec Pune ( Jan 2011 ---- Feb 2013 )</h1>
-	<div class="f5 dark-gray fw2 tracked nested-list-reset">
+	<h1 class="f3 lh-title">Symantec, Pune ( Jan 2011 ---- Feb 2013 )</h1>
+	<div class="f5 dark-gray fw2 tracked ">
 	<ul >
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray">Content Scanning API ( Enterprise Security )</a></li>
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray">Onboad Antivirus Scanning NetApp</a></li>
-	<li><a class="f5 fw7 dib no-underline bg-animate bg-white hover-bg-light-blue dark-gray">Data Collection SOAP client</a></li>
+	<li>Written SOAP client for Symantec Framework for Application data collection. (C++, SOAP, Linux, Windows)</li>
+	<li>Worked with NetApp on on-board Antivirus product. (Linux, C++)</li>
+	<li>Integrated Symantec micro definition system in On-board AV and scanning component.  (Linux, Networking, Curl, Windows, C++)</li>
+	<li>Individual contributor in Content Scanning API, part of Symantec Scan Engine.  (C++ Linux, Windows, Solaris)</li>
+	<li>Support different Symantec products for integration with content Scanning API, in both multithreaded and multiprocess scenario.</li>
 	</ul>
 	</div>
 	</div>
 
-	
+	<div class="center measure-wide f3 lh-copy ph2">
+	<h1 class="f3 lh-title">Persistent Systems, Pune ( April 2010 ---- Dec 2010)</h1>
+	<div class="f5 dark-gray fw2 tracked ">
+	<ul >
+	<li>Data manipulation component support. This was a support kind of profile in which, I use to fix issues on the product of Agilent. (C++, Linux)</li>
+	</ul>
+	</div>
+	</div>
+
+	<div class="center measure-wide f3 lh-copy ph2">
+	<h1 class="f3 lh-title">Clear-Trail , Noida ( August 2008 ---- Feb 2010)</h1>
+	<div class="f5 dark-gray fw2 tracked">
+	<ul >
+	<li>Developed Passive Lawful Interception system (Call and SMS). Used for running scan on existing data from STM1 link and trace out required data. (C++, Linux, Windows, Socket Programming, Design Patterns)</li>
+	<li>Active Lawful Interception System (Call and SMS). Used for gathering real-time data based on query and record the call based on that. (C++, Linux, Windows, Socket Programming, Design Patterns)</li>
+	</ul>
+	</div>
+	</div>
+
 
 
 	</article>`)
